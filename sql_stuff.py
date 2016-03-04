61df9d46f406190e4f9784a851f8570ac1a431da
Pedro Juarez <pdrjuarez@cs.uchicago.edu>
2016-03-03 23:08:20 -0600

    Some work on sql

--- sql_stuff.py
+++ sql_stuff.py
@@ -3,18 +3,23 @@ import csv
 import sqlite3
 import os
 import re
+ordered_columns={"crimes":
+                          ["date", "code", "location", "latitude", "longitude"],
+                 "IUCR_codes":
+                          ["code", "primary_type", "secondary_type"]
+                }
 
-#dictionary with {filename: (tuple containing [list containing tuples of the form (name of column imported, data type)], name of table }
+sql_datatypes={"crimes":
+                        {"date":"text", "code": "varchar(4)", "location": "varchar(50)", "latitude": "real", "longitude": "real"},
+               "IUCR_codes":
+                        {"code":"varchar(4)", "primary_type":"varchar(50)", "secondary_type": "varchar(50)"}
+               }
 
-crime_list=[("date","text"),("block","text"),("code", "varchar(4)"),("location_description","text"),("arrest","integer"),("domestic","integer"),("beat","text"),("district","text"),("ward","text"),("community_area","text"),("fbi_code","text"),("x_coord", "integer"),("y_coord", "integer"), ("lat", "real"), ("long", "real")]
-
-d={
-"IUCR_codes.csv":([("code", "varchar(4)"), ("primary_type", "text"), ("secondary_type", "text")],"IUCR_codes"),
-"crimes":(crime_list,"crimes")
-}
-
-BOOLEANS={"false": 0, "true": 1}
-
+LABELED_FILENAMES={"crimes":
+                            ["crimes_2013.csv", "crimes_2014.csv", "crimes_2015.csv", "crimes_2016.csv"],
+                   "IUCR_codes":
+                            ["IUCR_codes.csv"]
+                }
 
 
 my_lat=41.783213
@@ -22,16 +27,14 @@ my_long=-87.601375
 
 CHICAGO_AREA=606100000
 project_path=os.path.abspath(os.curdir)
-data_folder="/chicago_data/Clean/"
-full_path=project_path+data_folder
+csv_folder="/chicago_data/Clean/"
+csv_path=project_path+csv_folder
 
 def haversine(lon1, lat1, lon2, lat2):
     '''
     Calculate the circle distance between two points 
     on the earth (specified in decimal degrees)
     '''
-    if lon1=="" or lat1=="" or lon2=="" or lat2=="":
-        return -1
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
@@ -46,66 +49,69 @@ def haversine(lon1, lat1, lon2, lat2):
     m = km * 1000
     return m
 
+def db_helper(list_of_filenames, table_name, path=csv_path):
+    '''This is a function which takes in a list of filenames, a table_name for them. Returns the necessary
+    data and insertion/creation strings to import this into a database usable by sqlite3. Assumes all the files
+    are formatted similarly.'''
 
-
-
-def create_crimes_db_helper(list_of_filenames, path=full_path):
     data=[]
     for filename in list_of_filenames:
         with open(path+filename) as f:
             header=f.readline()
+            length=len(header.split(","))
             reader=csv.reader(f, delimiter=",")
             for row in reader:
+
                 if row==[]:
                     break
-                arrest,domestic=BOOLEANS[row[4]], BOOLEANS[row[5]]
-                data.append((row[0],row[1],row[2],row[3],arrest,domestic,row[6],row[7],row[8],row[9],row[10], row[11], row[12], row[13], row[14]))
+                if len(row)!=length:
+                    print("Warning: row {} has {} columns, but should have {}".format(row, len(row), length))
+                data.append(tuple(row))
 
-    create_column_string=", ".join([i[0] + " " + i[1] for i in d["crimes"][0]])
-    creation_string="CREATE TABLE crimes ("+create_column_string+");"
-    insert_column_string=", ".join([i[0] for i in d["crimes"][0]])
-    values=len(d["crimes"][0])
-    insertion_string="INSERT INTO "+ d["crimes"][1]+" ("+insert_column_string+") VALUES (?" +(", ?"*(values-1))+");"
+    create_column_string=", ".join([i + " " + sql_datatypes[table_name][i] for i in ordered_columns[table_name]])
+    creation_string="CREATE TABLE "+table_name+" ("+create_column_string+");"
+    insert_column_string=", ".join([i for i in ordered_columns[table_name]])
+    num_values=len(sql_datatypes[table_name])
+    insertion_string="INSERT INTO "+table_name+" ("+insert_column_string+") VALUES (?" +", ?"*(num_values-1)+");"
     return (data, creation_string, insertion_string)
 
 
-def create_crimes_db(lat,lon, distance, filename_list, path=full_path): 
-    ###used to create crime database
-    con=sqlite3.connect("crimes.db")
-    con.create_function("distance", 4, haversine)
+def create_db(labeled_filenames, database_name, path=csv_path): 
+    '''labeled filenames is a dictionary, with key value pairs as table_name: list of filenames under that table'''
+
+    con=sqlite3.connect(database_name)
     cur=con.cursor()
-    cur.execute("CREATE TABLE IUCR_codes (code varchar(4), primary_type varchar(50), secondary_type varchar(50));")
-    with open(path+"IUCR_codes.csv") as f:
-        header=f.readline()
-        reader=csv.reader(f, delimiter=",")
-        codes_data=[(row[0], row[1], row[2]) for row in reader if row!=[]]
-    cur.executemany("INSERT INTO IUCR_codes (code, primary_type, secondary_type) VALUES (?, ?, ?);", codes_data)
+    for j in labeled_filenames:
+        (data, creation_string, insertion_string)=db_helper(labeled_filenames[j], j)
+        cur.execute(creation_string)
+        print(insertion_string, data[3])
+        cur.executemany(insertion_string, data)
     con.commit()
 
-    (data,creation_string, insertion_string)=create_crimes_db_helper(filename_list)
-    cur.execute(creation_string)
-    cur.executemany(insertion_string, data)
-    con.commit()
-
-
-def crime_search(lat, lon, distance, path=full_path):
+def crime_search(lat, lon, distance, time, database_name):
     prop_area=pi*distance**2/CHICAGO_AREA
-    con=sqlite3.connect("crimes.db")
+    con=sqlite3.connect(database_name)
     con.create_function("distance", 4, haversine)
     cur=con.cursor()
-    sqlstring='''SELECT crimes.long, crimes.lat, date, primary_type, secondary_type FROM IUCR_codes JOIN crimes ON IUCR_codes.code=crimes.code WHERE distance({},{}, crimes.long, crimes.lat)<={}
-    AND distance({},{}, crimes.long, crimes.lat)>=0;'''.format(lon, lat, distance, lon, lat)
-
+    sqlstring='''SELECT date, primary_type, secondary_type, crimes.longitude, crimes.latitude FROM IUCR_codes JOIN crimes 
+    ON IUCR_codes.code=crimes.code WHERE (strftime('%s', 'now')-strftime('%s', date))<={} AND distance({},{}, crimes.longitude, crimes.latitude)<={};'''.format(time, lon, lat, distance)
+    #sqlstring=''' SELECT date, code, location, latitude, longitude FROM crimes'''
     cur.execute(sqlstring)
     results=cur.fetchall()
 
-    cur.execute('''SELECT * FROM IUCR_codes JOIN crimes ON IUCR_codes.code=crimes.code''')
-    total_results=cur.fetchall()
-    total_crimes=len(total_results)
+    cur.execute('''SELECT count(*) FROM crimes WHERE (strftime('%s', 'now')-strftime('%s',date))<={};'''.format(time))
+    total_crimes=cur.fetchall()[0][0]
+    if total_crimes==0:
+        return "Not enough results, please widen your time frame"
     num_crimes=len(results)
+    if num_crimes==0:
+        return ([], 10)
     prop_crimes=num_crimes/total_crimes
     crime_ratio=prop_crimes/prop_area
     return (results, crime_ratio)
+
+def ranking(lat, lon, distance, time):
+    pass
 
 
 
