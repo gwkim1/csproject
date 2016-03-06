94ff4d4342c44b9f09049e3ed7de9d574eed59cc
Ryan Liu <ryanliu3@cs.uchicago.edu>
2016-03-05 21:16:20 -0600

    asd

--- mysite/search/views.py
+++ mysite/search/views.py
@@ -13,21 +13,21 @@ def about(request):
 	return render(request, 'search/about.html', c)
 
 def homepage(request):
-    c = {}
-    c['names'] = 'Pedro, Eric, Ryan'
-    c['current_distance'] = request.POST.get('distance', 200)
-    c['current_loc']= request.POST.get('location', 60647)
-    c["current_price_limit"] =request.POST.get("price_limit", 1500)
-    attributes=["Restaurants nearby", "Active life nearby (parks, gyms, basketball courts)", "Arts and entertainment nearby","Schools/education nearby",
-    "Health establishments nearby (dentists, pharmacies)", "Nightlife nearby", "Shopping outlets nearby","Violent crime (homicide, assault, weapons violations, etc) nearby",
-    "Property crime (arson, theft, burglarly, criminal damage, etc) nearby", "Other victimed non-violent crime (tresspassing, stalking, etc) nearby", "Quality of life crime (gambling, narcotics, prostitution, etc) nearby"]
-    new_attributes=[]
-    count=0
-    for j in attributes:
-        count+=1
-        new_attributes.append([j,count])
-    c["survey"]=new_attributes
-    return render(request, 'search/home.html', c)
+	c = {}
+	c['names'] = 'Pedro, Eric, Ryan'
+	c['current_distance'] = request.POST.get('distance', 200)
+	c['current_loc']= request.POST.get('location', 60647)
+	c["current_price_limit"] =request.POST.get("price_limit", 1500)
+	attributes=["Restaurants nearby", "Active life nearby (parks, gyms, basketball courts)", "Arts and entertainment nearby","Schools/education nearby",
+	"Health establishments nearby (dentists, pharmacies)", "Nightlife nearby", "Shopping outlets nearby","Violent crime (homicide, assault, weapons violations, etc) nearby",
+	"Property crime (arson, theft, burglarly, criminal damage, etc) nearby", "Other victimed non-violent crime (tresspassing, stalking, etc) nearby", "Quality of life crime (gambling, narcotics, prostitution, etc) nearby"]
+	new_attributes=[]
+	count=0
+	for j in attributes:
+		count+=1
+		new_attributes.append([j,count])
+	c["survey"]=new_attributes
+	return render(request, 'search/home.html', c)
 
 
 def results(request):
@@ -42,7 +42,6 @@ def results(request):
 		weights.append(float(request.POST.get("pref_"+str(j))))
 
 	url=zillow.create_url(loc, listing_types=[listing_type], price_range= (0, price_limit), min_bedroom=0, min_bathroom=0, house_types= [house_type])
-	time=request.POST.get("time")
 	preferences=[]
 	for j in range(1,11):
 		preferences.append(float(request.POST.get("pref_"+str(j))))
@@ -52,20 +51,19 @@ def results(request):
 	result=zillow.get_house_info(soup, output_info=["latlong", "price", "address"])
 	Yelp_pref,database_pref=weights[:7],weights[7:]
 	list_of_house_coords=[(j[1],j[2]) for j in result]
-
 	Yelp_results=Yelp.get_yelp_scores(list_of_house_coords,distance,Yelp_pref)
-
+	scores=[]
 	database_results=sql_stuff.ranking(list_of_house_coords, "test.db",time,distance)
 	for j in range(len(Yelp_results)):
 		Yelp_results[j]+=database_results[j]
 		dot_product=0
-
 		for i in range(len(Yelp_results[j])):
 			dot_product+=Yelp_results[j][i]*weights[i]
 		scores.append(dot_product)
 		result[j].append(scores[j])
-	result.sort(key=lambda x: int(x[-1]))
-
+		print("property {} has score {}".format(result[j][0],scores[j]))
+	scores.sort()
+    ##figure out how to sort this based on the score coordinate
 	c={'results': result, "distance": distance}
 	return render(request, 'search/results.html', c)
 
